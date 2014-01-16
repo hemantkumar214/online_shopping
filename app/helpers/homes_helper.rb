69ac51e00b432e00b974141c527d0058c07e8d1b
hemant <hemant@neevtech.com>
2014-01-16 15:01:58 +0530

    syntax error resolved

--- app/helpers/homes_helper.rb
+++ app/helpers/homes_helper.rb
@@ -9,12 +9,13 @@ module HomesHelper
       fulltext (searching_keyword).strip
     end
     @category_results = @search_categories.results
-
+    
     # this code will search categories, if searched keyboard belongs to it.
     @search_products = Product.search do
       fulltext (searching_keyword).strip
     end
     @product_results = @search_products.results
+    
     return [@category_results, @product_results]
   end
   
